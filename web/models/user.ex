912c6a2b21a67092be8240bedf7d4d1490d824ee
Nicolas Bouhours <nico53960@hotmail.fr>
2017-09-13 18:55:34 +0200

    Comment model and migration

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,7 +6,8 @@ defmodule Discuss.User do
     field :provider, :string
     field :token, :string
     has_many :topics, Discuss.Topic
-    
+    has_many :comment, Discuss.Comment
+
     timestamps()
   end
 
